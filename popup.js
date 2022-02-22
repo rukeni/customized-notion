fc888a3abec226d4507585e2ed91c095c414b0ec
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-02-22 23:57:31 +0900

    Modify : excute script

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
+let script = document.getElementById("script");
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
@@ -11,10 +12,23 @@ changeColor.addEventListener("click", async () => {
   });
 });
 
+script.addEventListener("click", async (event) => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor2,
+  });
+})
+
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
+}
+
+function setPageBackgroundColor2() {
+    console.log('dd')
+    document.body.style.backgroundColor = 'red';
 }
